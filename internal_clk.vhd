1dca1f876eeb8c522b5ea8f2b6b92fa54ef8d625
fruzsi1 <101967617+fruzsi1@users.noreply.github.com>
2025-01-17 15:34:10 +0000

    Update internal_clk.vhd

--- internal_clk.vhd
+++ internal_clk.vhd
@@ -12,7 +12,7 @@ architecture Int_Osc_arch of Int_Osc is
 
 component OSCH
 -- synthesis translate_off
-  generic  (NOM_FREQ: string := "2.56");
+  generic  (NOM_FREQ: string := "2.08");
 -- synthesis translate_on
   port (STDBY    : in std_logic;
         OSC      : out std_logic;
@@ -20,14 +20,14 @@ component OSCH
 end component;
 
 attribute NOM_FREQ : string;
-attribute NOM_FREQ of OSCinst0 : label is "2.304"; -- original was "2.08"
+attribute NOM_FREQ of OSCinst0 : label is "2.08"; -- original was "2.08"
 -- 115200*20 = 2304000
    
 begin
 
   OSCInst0: OSCH
 -- synthesis translate_off
-    generic map( NOM_FREQ  => "2.304" ) -- original was "2.08"
+    generic map( NOM_FREQ  => "2.08" ) -- original was "2.08"
 -- synthesis translate_on
     port map (STDBY => StdBy, 
 	          OSC   => Clk);
